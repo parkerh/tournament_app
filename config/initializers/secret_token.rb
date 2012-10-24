dd5039a790dfe41b0240885fc5083b464a47b6f4
Parker Henry <parkerh@seas.upenn.edu>
2012-10-24 02:51:20 -0400

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TournamentApp::Application.config.secret_token = '865d4595d63077715e657eca9fe0d4e315847174e9dcc1b0c4c23adc293c7356538c9a347cfde39fd2b5c782203b0d9fb7a8eebaf3b483f4ffc6e022e5f5df00'
